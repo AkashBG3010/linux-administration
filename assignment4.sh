59e11c2681be406019a2c6461a995e4a116a4509
akashbpura@gmail.com <ubuntu@ip-172-31-45-122.ap-south-1.compute.internal>
2022-02-24 20:55:02 +0000

    Added to perform Disk management in linux

--- assignment4.sh
+++ assignment4.sh
@@ -1,3 +1,33 @@
 #!/bin/bash -x
 
+fdisk -l
 
+man fdisk
+
+fdisk /dev/xvdf
+
+man partprobe
+
+partprobe /dev/xvdf
+
+fdisk -l
+
+mkfs.ext4 /dev/xvdf1
+
+sudo su
+
+mkdir Data1 Data2 Data3 Data4
+
+mount /dev/xvdf1 /hone/ubuntu/Data1/
+
+df -h
+
+cd Data1
+
+dd if=/dev/xvdf1 of=1g.img bs=1 count=0 seek=2G
+
+while(true); do sleep 5s; done  > do ctrl-z
+
+df -H
+
+sudo umount /dev/xvdf1
